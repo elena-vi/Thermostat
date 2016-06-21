991fb08c75d5d97b83eb90e26ac96db82564a365
Elena <elena.vilimaite@gmail.com>
2016-06-21 17:16:03 +0100

    some basic functionality

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,23 +1,35 @@
-describe("Player", function() {
+'use strict';
+
+describe("Thermostat", function() {
   var thermostat;
 
   beforeEach(function() {
     thermostat = new Thermostat();
   });
 
-  it("", function() {
+  it("has base temperature 20", function() {
+    expect(thermostat.temperature()).toEqual(20);
+  });
 
+  it("temperature can be inceased", function() {
+    thermostat.increase();
+    expect(thermostat.temperature()).toEqual(21);
+  });
+
+  it("temperature can be deceased", function() {
+    thermostat.decrease();
+    expect(thermostat.temperature()).toEqual(19);
   });
 
   describe("", function() {
     beforeEach(function() {
     });
 
-    it("", function() {
+    xit("", function() {
     });
 
-    it("", function() {
+    xit("", function() {
     });
   });
-  
+
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,5 +1,23 @@
+'use strict';
+
 function Thermostat() {
-}
-class_name.prototype.method_name = function(first_argument) {
-	// body...
+	this._temp = 20;
+	this._minTemp = 10;
+	this._powerSaving = true;
+	this._maxTemp = (this._powerSaving) ? (25) : (35);
+};
+
+Thermostat.prototype = {
+	temperature: function() {
+		return this._temp;
+	}, 
+	reset: function() {
+		this._temp = 20;
+	}
+	increase: function() {
+		this._temp += 1;
+	}, 
+	decrease: function() {
+		this._temp -= 1;
+	} 
 };
