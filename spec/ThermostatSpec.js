4a861976a5c2f731bf9c3d07bd0e703472a269fa
Elena <elena.vilimaite@gmail.com>
2016-06-22 12:46:01 +0100

    full thermostat functionality

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,23 +1,85 @@
-describe("Player", function() {
+describe("Thermostat", function() {
   var thermostat;
 
   beforeEach(function() {
     thermostat = new Thermostat();
   });
 
-  it("", function() {
+  describe("defaults", function() {
+    it("it starts at 20 degrees", function() {
+      expect(thermostat.currentTemperature()).toEqual(20);
+    });
+
+    it("it minimum of 10 degrees", function() {
+      for (var i = 0; i < 11; i++) {
+        thermostat.decreaseTemperature();
+      };
+      expect(thermostat.currentTemperature()).toEqual(10);
+    });
+
+    it("Has power saving mode", function() {
+      expect(thermostat.powerSavingMode()).toEqual(true);
+    });
+
+    it("resets to 20", function() {
+      thermostat.increaseTemperature();
+      thermostat.resetTemperature();
+      expect(thermostat.currentTemperature()).toEqual(20);
+    });
+  });
+
+  describe("temperature changes", function() {
+    it("can increase", function() {
+      thermostat.increaseTemperature();
+      expect(thermostat.currentTemperature()).toEqual(21);
+    });
+
+    it("can decrease", function() {
+      thermostat.decreaseTemperature();
+      expect(thermostat.currentTemperature()).toEqual(19);
+    });
+  });
+
+  describe("maximum temperatures", function() {
+    it("power saving mode is on", function() {
+      for (var i = 0; i < 6; i++) {
+        thermostat.increaseTemperature();
+      };
+      expect(thermostat.currentTemperature()).toEqual(25);
+    });
+
+    it("power saving mode is off", function() {
+      thermostat.switchPowerSavingMode();
+      for (var i = 0; i < 13; i++) {
+        thermostat.increaseTemperature();
+      };
+      expect(thermostat.currentTemperature()).toEqual(32);
+    });
+  });
+
+  describe("thermostat colour", function() {
+
+    it("green if less than 18", function() {
+      for (var i = 0; i < 6; i++) {
+        thermostat.decreaseTemperature();
+      };
+      expect(thermostat.colour()).toEqual("green");
+    });
+
+    it("yellow if less than 25", function() {
+      for (var i = 0; i < 4; i++) {
+        thermostat.increaseTemperature();
+      };
+      expect(thermostat.colour()).toEqual("yellow");
+    });
+
+    it("red if more or equal to 25", function() {
+      for (var i = 0; i < 6; i++) {
+        thermostat.increaseTemperature();
+      };
+      expect(thermostat.colour()).toEqual("red");
+    });
 
   });
 
-  describe("", function() {
-    beforeEach(function() {
-    });
-
-    it("", function() {
-    });
-
-    it("", function() {
-    });
-  });
-  
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,5 +1,44 @@
 function Thermostat() {
+	this._temperature = 20;
+	this._minimum = 10;
+	this._powerSavingMode = true;
 }
-class_name.prototype.method_name = function(first_argument) {
-	// body...
+
+Thermostat.prototype.currentTemperature = function() {
+	return this._temperature;
 };
+
+Thermostat.prototype.increaseTemperature = function() {
+	if ((this.powerSavingMode() === true && this._temperature !== 25) ||
+		(this.powerSavingMode() === false && this._temperature !== 32)) {
+			this._temperature += 1
+		};
+	};
+
+	Thermostat.prototype.decreaseTemperature = function() {
+		if (this._temperature !== this._minimum) {
+			this._temperature -= 1
+		};
+	};
+
+	Thermostat.prototype.powerSavingMode = function() {
+		return this._powerSavingMode
+	};
+
+	Thermostat.prototype.switchPowerSavingMode = function() {
+		this._powerSavingMode = !this._powerSavingMode
+	};
+
+	Thermostat.prototype.resetTemperature = function() {
+		this._temperature = 20;
+	};
+
+	Thermostat.prototype.colour = function() {
+		if (this._temperature < 18) {
+			return 'green';
+		} else if (this._temperature < 25) {
+			return 'yellow';
+		} else {
+			return 'red';
+		};
+	};
